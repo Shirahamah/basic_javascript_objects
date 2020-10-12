c949ee4423c4538505f7df2a281bb6b972417719
Shirahamah <shirahama.hiroki@runneres.co.jp>
2020-10-12 11:50:15 +0900

    #1 scoresの要素にアクセス

---
+++ 02/js/main.js
@@ -0,0 +1,13 @@
+'use strict'
+
+{
+    const scores = [80, 90, 40];
+    // console.log(scores[1]);
+
+    scores[2] = 44;
+    console.log(scores);
+
+    // scores = 10;  scores への再代入不可、配列の要素への再代入は可
+
+    console.log(scores.length)
+}

68aae2a8d52ec0c3be2f16391a4175e873c9a20d
Shirahamah <shirahama.hiroki@runneres.co.jp>
2020-10-13 16:43:35 +0900

    #3 `map()`を使って配列の各要素に処理を追加して別の配列を作成

---
+++ 09/js/main.js
@@ -0,0 +1,13 @@
+'use strict'
+
+{
+    const prices = [180, 190, 200];
+    
+    // const updatePrices = prices.map((price) => {
+    //     return price + 20;
+    // });
+
+    const updatePrices = prices.map(price => price + 20);
+
+    console.log(updatePrices);
+}

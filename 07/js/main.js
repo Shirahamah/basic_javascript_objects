930696b863f55d98419815bc0892fb296fbd6a0b
Shirahamah <shirahama.hiroki@runneres.co.jp>
2020-10-13 15:13:51 +0900

    #3 分割代入、値の交換

---
+++ 07/js/main.js
@@ -0,0 +1,22 @@
+'use strict'
+
+{
+    const scores = [80, 90, 40, 70];
+
+    const [a, b, c, d] = scores;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);
+
+    const[e, f, ...others] =scores;
+    console.log(e);
+    console.log(f);
+    console.log(others);
+
+    let x = 30;
+    let y = 70;
+    [x, y] = [y, x];
+    console.log(x);
+    console.log(y);
+}

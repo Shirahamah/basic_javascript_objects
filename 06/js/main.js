59fde9bb41ad600a2977fb75f67b45e71ce4aa14
Shirahamah <shirahama.hiroki@runneres.co.jp>
2020-10-13 14:47:18 +0900

    #3 スプレッド構文を使って配列の中で別の配列を展開

---
+++ 06/js/main.js
@@ -0,0 +1,14 @@
+'use strict'
+
+{
+    const otherScores = [10, 20];
+    const scores = [80, 90, 40, 70, ...otherScores];
+    console.log(scores);
+
+    function sum(a, b) {
+        console.log(a + b);
+    }
+
+    sum(...otherScores);
+    // sum(10, 20); と同じ意味になる
+}

edd50bbd6adba35dd3e543b1e9fc88daa34152a1
Shirahamah <shirahama.hiroki@runneres.co.jp>
2020-10-22 16:20:30 +0900

    #7 join()で文字列の結合、split()で文字列の分割

---
+++ 18/js/main.js
@@ -0,0 +1,17 @@
+'use strict'
+
+{
+    const d = [2019, 11, 14];
+
+    console.log(d.join('/'));
+    console.log(d.join(''));
+
+    const t = '17:08:24'
+
+    console.log(t.split(':'));
+    const [hour, minute, second] = t.split(':');
+
+    console.log(hour);
+    console.log(minute);
+    console.log(second);
+}

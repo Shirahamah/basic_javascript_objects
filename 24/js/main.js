536125c231bb9e0ad8ecb732eb69b06eb3e4c137
Shirahamah <shirahama.hiroki@runneres.co.jp>
2020-10-26 15:59:52 +0900

    #9 setInterval()を使って現在日時を表示する関数を繰り返す

---
+++ 24/js/main.js
@@ -0,0 +1,15 @@
+'use strict'
+
+{
+    let i = 0;
+
+    function showTime() {
+        console.log(new Date());
+        i++;
+        if (i > 2) {
+            clearInterval(intervalId);
+        }
+    }
+
+    const intervalId = setInterval(showTime, 1000);
+}
